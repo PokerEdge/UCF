ac5b29ee5afa557c6b313d8e35aa96ffa30a94be
Reid <Reid@pokersprout.com>
2017-11-09 18:07:21 -0500

    Training area complete, Join A Plan started
    Navigation bar complete.
    
    Training area complete.
    
    Join A Plan area started and being designed.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // (function(){
 
+  const $window = $(window);
   //Toggle active class in navigation bar (and style it)
   $('.menu-item').on('click', function(){
 
@@ -23,9 +24,9 @@
 	});
 
   //Hide navigation menu on window resize if it was left shown at a 'mobile' width
-  $(window).resize(function() {
+  $window.resize(function() {
 
-    if ( $(window).width() >= 768 ){
+    if ( $window.width() >= 768 ){
       $('.hamburgler').removeClass('no-hamburgler');
       $('.sub-menu').css({"width":"0%","right":"-100%"});
     }
@@ -56,150 +57,6 @@
     //IF MOUSE OVER OTHER MENU-ITEM REMOVE HOVER STYLE
 
   });
-
-
-  // //BEGIN FIRST ALIGN HAMBURGER CODE
-  //
-  // //**** Navigation events ****
-  // $('.hamburger').click(function(){
-  //
-  //   $(this).toggleClass('is-active');
-  //
-  //   if($('.hamburger').hasClass('is-active')){
-  //
-  //     //Animate in sub-navigation events
-  //     $('.breadcrumb').hide();
-  //     $('.top-container nav').removeClass('nav-shadow');
-  //     $('.sub-nav').toggleClass('show-nav');
-  //
-  //   } else {
-  //     //Animate out sub-navigation events
-  //     $('.top-container nav').addClass('nav-shadow');
-  //     $('.sub-nav').removeClass('show-nav');
-  //     $('.breadcrumb').show();
-  //
-  //   }
-  //
-  // });
-  //
-  // //**** Sub-navigation events ****
-  //
-
-    //Use forEach() to initialize
-
-  // //Initialize sub-navigation
-  // $('.homeMenu').hide();
-  // $('.latestNewsMenu').hide();
-  // $('.servicesMenu').hide();
-  // $('.industriesMenu').hide();
-  // $('.careersMenu').hide();
-  // $('.aboutMenu').hide();
-  //
-  // //Click handlers for each sub-navigation menu list item
-  // $('.home').click(function(){
-  //   $('.sub-nav-list-elements a').removeClass('activeSubNav');
-  //   $(this).addClass('activeSubNav');
-  //   //Add hover effects to all nav elements
-  //
-  //   //Remove hover effect from THIS element
-  //
-  //   //Dynamically style page for "latest news" menu
-  //   $('.servicesMenu').hide();
-  //   $('.industriesMenu').hide();
-  //   $('.careersMenu').hide();
-  //   $('.aboutMenu').hide();
-  //   $('.latestNewsMenu').hide();
-  //   $('.homeMenu').show();
-  //
-  // });
-  //
-  // $('.latestNews').click(function(){
-  //   $('.sub-nav-list-elements a').removeClass('activeSubNav');
-  //   $(this).addClass('activeSubNav');
-  //   //Add hover effects to all nav elements
-  //
-  //   //Remove hover effect from THIS element
-  //
-  //   //Dynamically style page for "latest news" menu
-  //   $('.homeMenu').hide();
-  //   $('.servicesMenu').hide();
-  //   $('.industriesMenu').hide();
-  //   $('.careersMenu').hide();
-  //   $('.aboutMenu').hide();
-  //   $('.latestNewsMenu').show();
-  //
-  // });
-  //
-  // $('.services').click(function(){
-  //   $('.sub-nav-list-elements a').removeClass('activeSubNav');
-  //   $(this).addClass('activeSubNav');
-  //
-  //   //Dynamically style page for "services" menu
-  //
-  //   //Remove hover effect from THIS element
-  //
-  //   //Dynamically style page for "latest news" menu
-  //   $('.homeMenu').hide();
-  //   $('.industriesMenu').hide();
-  //   $('.careersMenu').hide();
-  //   $('.aboutMenu').hide();
-  //   $('.latestNewsMenu').hide();
-  //   $('.servicesMenu').show();
-  // });
-  //
-  // $('.industries').click(function(){
-  //   $('.sub-nav-list-elements a').removeClass('activeSubNav');
-  //   $(this).addClass('activeSubNav');
-  //
-  //   //Dynamically style page for "industries" menu
-  //
-  //   //Remove hover effect from THIS element
-  //
-  //   //Dynamically style page for "latest news" menu
-  //   $('.homeMenu').hide();
-  //   $('.careersMenu').hide();
-  //   $('.aboutMenu').hide();
-  //   $('.latestNewsMenu').hide();
-  //   $('.servicesMenu').hide();
-  //   $('.industriesMenu').show();
-  // });
-  //
-  // $('.careers').click(function(){
-  //   $('.sub-nav-list-elements a').removeClass('activeSubNav');
-  //   $(this).addClass('activeSubNav');
-  //
-  //   //Dynamically style page for "careers" menu
-  //
-  //   //Remove hover effect from THIS element
-  //
-  //   //Dynamically style page for "latest news" menu
-  //   $('.homeMenu').hide();
-  //   $('.aboutMenu').hide();
-  //   $('.latestNewsMenu').hide();
-  //   $('.servicesMenu').hide();
-  //   $('.industriesMenu').hide();
-  //   $('.careersMenu').show();
-  // });
-  //
-  // $('.about').click(function(){
-  //   $('.sub-nav-list-elements a').removeClass('activeSubNav');
-  //   $(this).addClass('activeSubNav');
-  //
-  //   //Dynamically style page for "about" menu
-  //
-  //   //Remove hover effect from THIS element
-  //
-  //   //Dynamically style page for "latest news" menu
-  //   $('.homeMenu').hide();
-  //   $('.latestNewsMenu').hide();
-  //   $('.servicesMenu').hide();
-  //   $('.industriesMenu').hide();
-  //   $('.careersMenu').hide();
-  //   $('.aboutMenu').show();
-  // });
-  // //END FIRST ALIGN HAMBURGER CODE
-
-
 
   //Manage navigation placement when alert is closed
   $('#ucfAlert').on('closed.bs.alert', function() {
