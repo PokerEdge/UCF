8c5f01e64014593f6c96b202293e08227f5a7b29
Reid <Reid@pokersprout.com>
2017-11-29 16:05:10 -0500

    Static calendar styles populated. Filter functionality refactoring and styling started. Refactored JavaScript.
    Static calendar styles populated. Filter functionality refactoring and
    styling started. Refactored JavaScript.
    
    Next step is to complete filtering functionality and then to complete
    styling for schedules page, including all calendar related and
    surrounding styles.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 // (function(){
 
-  //HIDE CLASS NAME OF PARTICULAR TYPE FOR FILTER
-  // $('.class-oly').hide();
-  // $('.class-crossfit').hide();
-  // $('.class-openGym').hide();
-  // $('.class-conditioning').hide();
+  'use strict';
 
+  // Viewport width delaration to eliminate responsive bug on quick resize with open mobile menu
   const $window = $(window);
+
+  //Filter helper classes
+  const $crossfit = $('.class-crossfit');
+  const $oly = $('.class-oly');
+  const $openGym = $('.class-openGym');
+  const $conditioning = $('.class-conditioning');
+
+  //Add formated current date to top of calendar message
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const date = new Date().toLocaleString('en-US', options);
+  $('#currentDate').append(date);
+
   //Toggle active class in navigation bar (and style it)
   $('.menu-item').on('click', function(){
 
@@ -38,8 +47,10 @@
     }
   });
 
-  //Open contact links in new tab or window on click
-  $('.contact-link').each(function() {
+  // Open contact links in new tab or window on click
+  const $contactLink = $('.contact-link');
+
+  $contactLink.each(function() {
    var a = new RegExp('/' + window.location.host + '/');
    if(!a.test(this.href)) {
        $(this).click(function(e) {
@@ -50,7 +61,10 @@
     }
   });
 
-  $('.get-started').each(function() {
+  const $getStarted = $('.get-started');
+
+  // Open get started links in new tab or window on click
+  $getStarted.each(function() {
    var a = new RegExp('/' + window.location.host + '/');
    if(!a.test(this.href)) {
        $(this).click(function(e) {
@@ -59,6 +73,17 @@
            window.open(this.href, '_blank');
        });
     }
+  });
+
+  /*************************************
+  **** MANAGE DROPDOWN MENU ACTIONS ****
+  *************************************/
+
+  //IF MOUSE OVER OTHER MENU-ITEM REMOVE HOVER STYLE
+  $('.menu-item').on('mouseover', function(){
+    $('.dropdown-menu').removeClass('show');
+    $('.fa-caret-down').removeClass('caret-hover');
+    $('.dropdown-toggle').css({"color": "rgba(0, 0, 0, 0.5)"});
   });
 
   //Open menu toggle item on mouse over
@@ -74,34 +99,26 @@
   });
 
   $('.dropdown-menu').on('mouseleave', function(){
-
     $(this).toggleClass('show');
     $('.dropdown-toggle').css({"color":"rgba(0,0,0,0.5)"});
-
-    //reverse animate arrow with a css transition class
-
-    //USE CLASS THAT ISN'T FA-CARET-DOWN
     $('.fa-caret-down').removeClass('caret-hover');
-
-    //IF MOUSE OVER OTHER MENU-ITEM REMOVE HOVER STYLE
-
   });
 
   //CALENDAR PAGE DROPDOWN
   //Open menu toggle item on mouse over
-  $('.calendar-dropdown-toggle').on('mouseenter', function(){
+  $('.calendar-dropdown-toggle').on('click', function(){
 
-    $('.calendar-dropdown-menu').toggleClass('show');
-    $(this).css({"color":"#f27126"});
+    $('.calendar-dropdown-menu').show();
+    // $(this).css({"color":"#f27126"});
 
-    //animate arrow with a css transition class (add arrow next to it as a span and rotate it 180 deg.)
+    //MANAGE THESE CLASS NAMES TO KEEP CARET ANIMATION INDEPEDENT FROM NAV CARET
     $('.calendar-caret').addClass('caret-hover');
-
   });
 
   $('.calendar-dropdown-menu').on('mouseleave', function(){
 
-    $(this).toggleClass('show');
+    //MAKE DISPLAY NONE A HELPER CLASS
+    $(this).hide();
     //
     $('.calendar-dropdown-toggle').css({"color":"rgba(0,0,0,0.5)"});
 
@@ -111,6 +128,44 @@
     //IF MOUSE OVER OTHER MENU-ITEM REMOVE HOVER STYLE
 
   });
+
+
+  //HIDING CLASS NAME OF PARTICULAR TYPE TESTING FOR FILTERING FUNCTIONALITY
+
+  // $crossfit.hide();
+  // $oly.hide();
+  // $openGym.hide();
+  // $conditioning.hide();
+  // $oly.hide();
+
+  $('.dropdown-item').on('click', function(){
+
+    if(){
+      // $crossfit.hide();
+      // $oly.hide();
+      // $openGym.hide();
+      // $conditioning.hide();
+    } else if() {
+      // $crossfit.hide();
+      // $oly.hide();
+      // $openGym.hide();
+      // $conditioning.hide();
+    } else if() {
+      // $crossfit.hide();
+      // $oly.hide();
+      // $openGym.hide();
+      // $conditioning.hide();
+    } else if(){
+      // $crossfit.hide();
+      // $oly.hide();
+      // $openGym.hide();
+      // $conditioning.hide();
+    }
+
+  });
+  // HIDE CALENDAR DROPDOWN WHEN LINK IS CLICKED
+    // TAKE FILTER ACTION TO HIDE ALL BUT CLICKED LINK ITEM IN DROPDOWN
+
 
   //Manage navigation placement when alert is closed
   $('#ucfAlert').on('closed.bs.alert', function() {
